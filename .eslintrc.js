9bb3b464a4e052cc62a6c5426c4b259a37f726a5
olivier <17858343@qq.com>
2017-11-07 13:42:58 +0800

    edit setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,10 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // 不检测新文件末尾是否有空行
+    'eol-last': 0,
+    // 不在函数括号左侧加空格
+    'space-before-function-paren': 0
   }
 }
