73ad9ed1a6bb2c7f1bd68da4d298515b73134009
olivier <17858343@qq.com>
2017-11-09 01:41:07 +0800

    change tab to component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <template>
   <div id="app">
     <v-header></v-header>
-    <div class="tab">
-      <router-link tag="div" class="tab-item" to="/goods">
-        <span class="tab-link">商品</span>
-      </router-link>
-      <router-link tag="div" class="tab-item" to="/ratings">
-        <span class="tab-link">评价</span>
-      </router-link>
-      <router-link tag="div" class="tab-item" to="/seller">
-        <span class="tab-link">商家</span>
-      </router-link>
-    </div>
+    <tab></tab>
     <keep-alive>
       <router-view/>
     </keep-alive>
@@ -20,33 +10,20 @@
 
 <script>
 import header from './components/header/header.vue'
+import Tab from './components/tab/tab.vue'
+
 export default {
   name: 'app',
   components: {
     // 不能直接使用header 和原生的 h5标签 冲突
-    'v-header': header
+    'v-header': header,
+    Tab
   }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  // vue-loader 带有的 postcss 插件 会自动解决大部分浏览器兼容问题
-  #app
-    .tab
-      display: flex
-      width: 100%
-      // 设计稿高度80px 设备上是375px 物理像素750px
-      // 所以设计稿也是按两倍标记的 码的时候知道就行
-      height: 40px
-      line-height: 40px
-      .tab-item
-        flex: 1
-        text-align: center
-        display: block
-        .tab-link
-          color: rgb(77,85,93)
-          font-size: 14px
-        &.router-link-active
-          .tab-link
-            color: rgb(240,20,20)
+  // vue-loader 带有的 postcss 插件 会自动解决大部分浏览器兼容问题  
+  // 将 localhost 改为 本机ip地址 然后上 草料网 复制地址生成二维码 在同个局域网内的手机可以扫描二维码看结果
+  // window下是 cmd --> ipconfig wifi连接就是IPv4地址
 </style>
---
+++ src/components/tab/tab.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="tab border-1px">
+    <router-link tag="div" class="tab-item" to="/goods">
+      <span class="tab-link">商品</span>
+    </router-link>
+    <router-link tag="div" class="tab-item" to="/ratings">
+      <span class="tab-link">评价</span>
+    </router-link>
+    <router-link tag="div" class="tab-item" to="/seller">
+      <span class="tab-link">商家</span>
+    </router-link>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    name: 'tab'
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+
+  .tab
+    display: flex
+    width: 100%
+    // 设计稿高度80px 设备上是375px 物理像素750px
+    // 所以设计稿也是按两倍标记的 码的时候知道就行
+    height: 40px
+    line-height: 40px
+    border-1px-bottom(rgba(7,17,27,0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+      display: block
+      .tab-link
+        color: rgb(77,85,93)
+        font-size: 14px
+      &.router-link-active
+        .tab-link
+          color: rgb(240,20,20)
+</style>
